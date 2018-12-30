b6243ca87b7644fe747a8d605712f043fbf73bef
stephenkk <44733728+stephenkk@users.noreply.github.com>
2018-12-29 21:50:51 -0500

    Update TestResSystem.java

--- TestResSystem.java
+++ TestResSystem.java
@@ -1,13 +1,1 @@
 
-public class TestResSystem {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		ResSystem flight = new ResSystem();
-		flight.setVisible(true);
-		
-		
-	}
-
-}
